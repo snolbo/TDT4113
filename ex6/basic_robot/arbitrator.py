d40816d4ec34622e747ff3f40c577f103620c04e
snolbo <snorrebortnes.com>
2017-11-03 11:07:04 +0100

    Start ex6 robot

--- ex6/basic_robot/arbitrator.py
+++
@@ -1,6 +0,0 @@
-
-
-class Arbitrator:
-
-    def __init__(self, bbcon):
-        pass
--- robot/bbcon.py
+++ robot/bbcon.py
@@ -1,64 +1,66 @@
-
+from time import sleep
 
 class BBCON:
-    def __init__(self):
+    def __init__(self, arbitrator,  sensobs=[], motobs=[], behaviors=[], active_behaviors=[]):
+        self.sensobs = sensobs
 
-        # List of  behaciors used by BBCON
-        self.behaviors = []
-        self.active_behaviors = []
-
-
-        # Lists of sensor objects and motor object used by BBOCN
-        self.sensobs = []
-        self.motobs = []
+        self.behaviors = behaviors
+        self.motobs = motobs
+        self.active_behaviors = active_behaviors
 
         # Object to resolve actuator request produced by behaviors
-        self.arbitrator = None
+        self.arbitrator = arbitrator
 
-        # Information about timesteps
         self.timestep_length = 0.5
-        self.current_timestep = 0
 
 
-    ## METHOD BBCON SHOULD HAVE
-    def add_behavior(self, behavior):
+    def addBehavior(self, behavior):
         if behavior not in self.behaviors:
             self.behaviors.append(behavior)
 
-    def add_sensob(self, sensob):
+    def addSensob(self, sensob):
         if sensob not in self.sensobs:
             self.sensobs.append(sensob)
 
-    def activate_behavior(self, behavior):
+    def activateBehavior(self, behavior):
         if behavior not in self.active_behaviors:
             self.activate_behaviors.append(behavior)
 
-    def deactivate_behavior(self, behavior):
+    def deactivateBehavior(self, behavior):
         if behavior in self.active_behaviors:
             self.active_behaviors.remove(behavior)
 
+
     ## METHOD BBCON MUST HAVE
-    def run_one_timestep(self):
+    def runOneTimestep(self):
 
         # 1. update all sensobs, query for values and pre-proccessing
+        for sensob in sensobs:
+            sensob.update()
 
 
         # 2. update all behaviors. read relevant sensobs values and produce motor recomendation
-
+        for behavior in self.behaviors:
+            behavior.update()
 
 
         # 3. invoke arbitrator -> arbitrator.choose_action() that chooses and return winning behavior's
-        #    motor reccomendations and helt_request flag
+        #    motor reccomendations and halt_requests
 
 
         # 4. update all motobs based on these reccomendations, motobs updates settings on all motors
 
 
         # 5 Wait time step to let motor perform the chosen action
+        time.sleep(self.timestep_length)
 
 
         # 6 reset sensobs
-        for ob in self.sensobs:
+        for sensob in self.sensobs:
+            sensob.resetSensors()
+
+
+
 
 
 
